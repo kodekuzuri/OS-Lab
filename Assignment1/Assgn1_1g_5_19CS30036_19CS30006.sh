0eaeea7a03d1eb29e63cbe298941ece5b228952d
Pranav Rajput <57314391+kodekuzuri@users.noreply.github.com>
2022-01-22 22:29:40 +0530

    Completed 1g

--- Assignment1/Assgn1_1g_5_19CS30036_19CS30006.sh
+++ Assignment1/Assgn1_1g_5_19CS30036_19CS30006.sh
@@ -4,6 +4,10 @@ for i in $(seq 1 150)
 do
     echo "$RANDOM,$RANDOM,$RANDOM,$RANDOM,$RANDOM,$RANDOM,$RANDOM,$RANDOM,$RANDOM,$RANDOM" >> $1
 done
-cut -d ',' -f $2 $1 
+if [[ $(cut -d ',' -f $2 $1) =~ $3 ]]
+then
+    echo "YES"
+else 
+    echo "NO"
+fi 
 
-# matching pending 
