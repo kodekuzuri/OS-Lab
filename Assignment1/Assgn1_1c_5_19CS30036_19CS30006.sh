c74d2105929fa0254e2357eff50c7b4618926035
victorvini08 <56232806+victorvini08@users.noreply.github.com>
2022-01-23 18:30:27 +0530

    Update Assgn1_1c_5_19CS30036_19CS30006.sh

--- Assignment1/Assgn1_1c_5_19CS30036_19CS30006.sh
+++ Assignment1/Assgn1_1c_5_19CS30036_19CS30006.sh
@@ -1,1 +1,11 @@
-#pending 
+#!/bin/bash
+my_array=($(find . -type f | sed -rn 's|.*/[^/]+\.([^/.]+)$|\1|p' | sort -u))
+for i in "${my_array[@]}"
+do 
+	echo "$i"   # Used for debugging
+	mkdir -p $i
+	find . -name "*.$i" -type f -not -path "./$i/*" -exec mv -n -t "$i" {} \;
+done
+echo "No extension files" # Used for debugging
+mkdir -p "No_Extension"
+find . -type f ! -name "*.*" -not -path "./No_Extension/*" -exec mv -n -t "No_Extension" {} + 
