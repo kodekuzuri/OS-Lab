327181c70711b8ddcf6217514090a89ac87d3b6f
victorvini08 <56232806+victorvini08@users.noreply.github.com>
2022-02-08 16:02:53 +0530

    Added search history command

--- Assignment 2/Assignment2_5_19CS30006_19CS30036.c
+++ Assignment 2/Assignment2_5_19CS30006_19CS30036.c
@@ -4,9 +4,15 @@
 #include "sys/wait.h"
 #include "fcntl.h"
 #include <string.h>
+#include <signal.h>
+#include <termios.h>
 
 #define BUFFER 512
+#define MAX_HISTORY_LENGTH 10000
+#define MAX_COMMAND_LENGTH 512
 
+char history[MAX_HISTORY_LENGTH+1][MAX_COMMAND_LENGTH];  // To read history from file to array
+int current_history_length;
 
 // Builtin functions
 int builtin_help(char** args);
@@ -39,11 +45,156 @@ int builtin_exit(char** args)
 {
     return 1;
 }
+
+void sig_int_handler(int sig_no)
+{
+	printf("\nCommand execution stopped\n");
+	fflush(stdout);
+}
+
+void sig_tstp_handler(int sig_no)
+{
+	printf("\n Command moved to background\n");
+	fflush(stdout);
+}
+
+void add_to_history(char* line)
+{
+	if(current_history_length<MAX_HISTORY_LENGTH)
+	{
+		for (int i = current_history_length; i >0; i--)
+		{
+			/* code */
+			strcpy(history[i],history[i-1]);
+		}
+		current_history_length++;
+		strcpy(history[0],line);
+	}
+	else
+	{
+		for (int i = current_history_length; i >0; i--)
+		{
+			/* code */
+			strcpy(history[i],history[i-1]);
+		}
+		strcpy(history[0],line);
+	}
+}
+void show_history()
+{
+	printf("The most recent  commands are given below: \n");
+	if(current_history_length<1000)
+	{
+		for (int i = 0; i < current_history_length; ++i)
+	{
+		/* code */
+		printf("%s\n",history[i] );
+	}	
+	}
+	else
+	{
+	for (int i = 0; i < 1000; ++i)
+	{
+		/* code */
+		printf("%s\n",history[i] );
+	}
+	}
+}
+
+int max_num(int num1, int num2)
+{
+    return (num1 > num2 ) ? num1 : num2;
+}
+
+int longest_substring(char* X, char* Y, int m, int n)
+{
+    
+ 
+    int LCSuff[m + 1][n + 1];
+    int result = 0; // To store length of the
+                    // longest common substring
+ 
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            
+            if (i == 0 || j == 0)
+                LCSuff[i][j] = 0;
+ 
+            else if (X[i - 1] == Y[j - 1]) {
+                LCSuff[i][j] = LCSuff[i - 1][j - 1] + 1;
+                result = max_num(result, LCSuff[i][j]);
+            }
+            else
+                LCSuff[i][j] = 0;
+        }
+    }
+    return result;
+}
+ 
+void search_history()
+{
+	printf("Enter search term\n");
+    char term[512];
+    scanf("%s",term);
+	int flag = 0;
+	for (int i = 0; i < current_history_length; ++i)
+	{
+		/* code */
+		if(strcmp(history[i],term)==0)
+		{
+			printf("Most recent command matching the prompt is Number %d and is %s\n",i+1,history[i] );
+			flag = 1;
+			break;
+		}
+	}
+	if(flag)
+	{
+		return;
+	}
+	else
+	{
+		int max = 0;
+		int ans = 0;
+		char* curr_command = "";
+		for (int i = 0; i < current_history_length; ++i)
+		{
+			/* code */
+			int m = strlen(term);
+			int n = strlen(history[i]);
+			int curr_length = longest_substring(term,history[i],m,n);
+			if(curr_length>max)
+			{
+				max = curr_length;
+				ans   = i;
+				curr_command = history[i];
+			}
+		}
+		if(max<=2)
+		{
+			printf("No match for search term in history\n");
+		}
+		else
+		{
+			printf("Longest substring match was found in Command number %d which is %s\n",ans,curr_command);
+		}
+	}
+}
 int read_line(char* line)
 {
 	int buffer_size = BUFFER;
 	int c;
 	int i = 0;
+
+	// termios is used to detect ctrl+R and tab characters 
+	struct termios old,new;
+	tcgetattr(STDIN_FILENO,&old);
+	new = old;
+	new.c_lflag &= ~(ICANON | ECHO);
+	new.c_cc[VMIN] = 1;
+	new.c_cc[VTIME] = 0;
+	tcsetattr(STDIN_FILENO,TCSANOW,&new);
 
 	while(1)
 	{
@@ -52,10 +203,36 @@ int read_line(char* line)
 		if(c==EOF || c=='\n')
 		{
 			line[i] = '\0';
+			printf("\n");
+			tcsetattr(STDIN_FILENO,TCSANOW,&old);
 			return EXIT_SUCCESS;
 		}
+		else if(c==18)   // CTRL + R check
+		{
+			tcsetattr(STDIN_FILENO,TCSANOW,&old);
+			search_history();
+			return EXIT_SUCCESS;
+		}
+		else if(c==127) // Backspace check
+		{
+			if(i>0)
+			{
+				printf("\b \b");
+				line[i] = '\0';
+				i--;
+			}
+			else
+				continue;
+		}
+		else if(c=='\t') // Tab check
+		{
+			continue;
+		}
+		else
+		{
 		line[i++] = c;
-
+		printf("%c",c);
+		} 
 		// Since we dont know how much memory would be needed for line, we keep on reallocating more memory as and when it exceeds.
 		if(i >= buffer_size)
         {
@@ -185,7 +362,7 @@ int external_command(char** arguments,int fdIN, int fdOUT, int n)
             // WEXITSTATUS: exit status (normally/abnormally)
 
             // keep waiting until process hasn't exited and is behaving normally 
-            while((WIFSIGNALED(s) == 0) && (WEXITSTATUS(s) == 0)){
+            while((WIFSIGNALED(s) == 0) && (WIFEXITED(s) == 0)){
                 wait_PID = waitpid(processID, &s, WUNTRACED) ;
             }
         }
@@ -193,6 +370,7 @@ int external_command(char** arguments,int fdIN, int fdOUT, int n)
 
     //child process
     else if(!processID){
+
 
         // flag to check symbol commands (<,>,&), lenC to store the length of commands 
         int flag = 0 , lenC = 0, k = 0, i = 0 ; 
@@ -280,6 +458,13 @@ int run_command(char** arguments, int n){
     
     // if arguments are presents, execute 
     if(arguments[0] != NULL){
+
+    	// For history command 
+    	 if(strcmp(arguments[0],"history")==0)
+    	 {
+    	 	show_history();
+    	 	return EXIT_SUCCESS;
+    	 }
         int i = 0 ; 
         int num_builtins = num_builtin_functions() ; 
 
@@ -309,23 +494,56 @@ void command_loop()
 	do
 		{
 			/* code */
+
+			signal(SIGINT,sig_int_handler);
+			signal(SIGTSTP,sig_tstp_handler);
 			line = (char *)malloc(sizeof(char)*BUFFER);
 			status = read_line(line);
+			int l = strlen(line);
+			if(l==0)
+				continue;
 			args = (char**)malloc(sizeof(char*)*BUFFER);
 			num_args = split_line(args,line);
+			if(args[0]==NULL)
+				continue;
 			if(args!=NULL)
+			{
+				add_to_history(line);
 			status = run_command(args,num_args);
-
+			}
 			free(line);
 			free(args);
-
+			fflush(stdin);
+       	    fflush(stdout);
 		} while (status==EXIT_SUCCESS);	
 }
 
 int main(int argc, char const *argv[])
 {
+	char *filename = "history.txt";
+	int pos = 0;
+	FILE *fptr = fopen(filename,"r");
+	if(fptr)
+	{
+		// fseek(fptr,0,SEEK_END);
+		// long size = ftell(fptr);
+		// printf("%ld\n",size );
+	while(fgets(history[pos],MAX_COMMAND_LENGTH,fptr))
+	{
+		history[pos][strlen(history[pos])-1] = '\0';
+		pos++;
+	}
+	current_history_length = pos;
+	fclose(fptr);
+	}
     printf("#################Welcome to my shell!!#################\n");
-    printf("#################Sussy Baka Lesss gooooo#################\n") ; 	
     command_loop();
+    FILE *fptr2 = fopen(filename,"w");
+    for (int i = 0; i < current_history_length; ++i)
+    {
+    	/* code */
+    	fprintf(fptr2,"%s\n",history[i]);
+    }
+    fclose(fptr2);
 	return 0;
 }
