fa63da7fa00aafd62dfe79fb074d5a68a1e97786
victorvini08 <56232806+victorvini08@users.noreply.github.com>
2022-03-25 13:56:23 +0530

    Update memlab.cpp

--- Assignment 5/memlab.cpp
+++ Assignment 5/memlab.cpp
@@ -1,8 +1,16 @@
 #include "memlab.h"
 
+vector<int> var_table;
+
+int size_of_datatype(int var_type)
+{
+	int datatype_sizes[4] = {32,8,24,1};
+
+	return datatype_sizes[var_type];
+}
 void* createMem(int memory)
 {
-	void *p = malloc(memory);
+	void *p = malloc(memory*8); //malloc takes mem in bytes
 	if(p==NULL)
 	{
 		perror("malloc");
@@ -11,9 +19,12 @@ void* createMem(int memory)
 	return p;
 }
 
-void createVar(int var_size)
+Datatype createVar(int var_type)
 {
 	// Find free block
+	int size = size_of_datatype(var_type);
 
 	// Allocate space for variable
+
+	return Datatype(local_index,var_type);
 }
