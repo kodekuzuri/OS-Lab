65d1ca6462e35ae7a56347c39786896ad0387b33
Pranav Rajput <57314391+kodekuzuri@users.noreply.github.com>
2022-03-25 14:52:18 +0530

    Update memlab.cpp

--- Assignment 5/memlab.cpp
+++ Assignment 5/memlab.cpp
@@ -1,6 +1,8 @@
 #include "memlab.h"
 
-vector<int> var_table;
+map<int,int> var_table;
+block *mem ; 
+
 
 int size_of_datatype(int var_type)
 {
@@ -8,23 +10,52 @@ int size_of_datatype(int var_type)
 
 	return datatype_sizes[var_type];
 }
-void* createMem(int memory)
+
+void createMem(int memory)
 {
-	void *p = malloc(memory*8); //malloc takes mem in bytes
+	void *p = malloc(memory); //malloc takes mem in bytes
 	if(p==NULL)
 	{
 		perror("malloc");
 		exit(0);
 	}
-	return p;
+ 
+	mem = (block *) p ;  
+	mem->size = memory ;
+	mem->next = NULL ;
+	mem->free_flag = 1 ; 
+	mem->data = NULL ; 
+	mem->padding = 0 ; 
+
+	cout << p << "\n" ; 
+	cout << mem << "\n" ; 
+	cout << mem->size << "\n" ; 
+	return ;
 }
 
 Datatype createVar(int var_type)
 {
 	// Find free block
-	int size = size_of_datatype(var_type);
+	int size = size_of_datatype(var_type); // size is in bits
+	int local_index = var_table.size() ; 
 
+	// word Id 
+	int wordId = mem->getMemory(size);
+	var_table.push_back(wordId);
+
+	
 	// Allocate space for variable
 
 	return Datatype(local_index,var_type);
+}
+
+
+int main(){
+	int size ; 
+
+	cout << "Enter Size : " << "\n" ; 
+	cin >> size ; 
+
+
+	createMem(size) ; 
 }
